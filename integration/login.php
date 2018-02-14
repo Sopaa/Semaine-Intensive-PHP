ad989524961850b370aea315bc650d5af7ebbbc9
Sopaa <pierre.bourgeois@hetic.net>
2018-02-14 16:37:26 +0100

    structure reorganisation

--- integration/login.php
+++ integration/login.php
@@ -6,25 +6,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="shortcut icon" href="meatLogo.ico">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/login.css">
+  <link rel="shortcut icon" href="../img/ViandeLogo.png">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/login.css">
   <title>Meat</title>
 </head>
 
 <body>
   <div class="content">
-    <img class="logo" src="img/ViandeLogo.png" alt="Logo">
+    <img class="logo" src="../img/ViandeLogo.png" alt="Logo">
     <div class="formContainer">
 
       <h1 class="title">Bienvenue sur Meat.</h1>
 
 
-      <form class="" action="index.html" method="post">
+      <form class="" action="homepage.php" method="post">
         <div class="formFlex">
           <input class="loginInput" type="email" name="id" value="" placeholder="Votre Email">
           <input class="loginInput" type="password" name="password" value="" placeholder="Votre mot de passe">
-          <p class="formText">Vous n’avez pas de compte ? <strong><a class="Register" href="#">Inscrivez-vous !</a></strong></p>
+          <p class="formText">Vous n’avez pas de compte ? <strong><a class="Register" href="register.php">Inscrivez-vous !</a></strong></p>
           <input class="submitInput" type="submit" name="sumbit" value="Se Connecter">
         </div>
       </form>
@@ -33,13 +33,13 @@
 
       <div class="footer">
         <div class="socialContainer">
-          <a href="#"><img class="socialImg" src="img/amazon.png" alt="Amazon"></a>
-          <a href="#"><img class="socialImg" src="img/facebook.png" alt="Facebook"></a>
-          <a href="#"><img class="socialImg" src="img/twitter.png" alt="Twitter"></a>
+          <a href="#"><img class="socialImg" src="../img/amazon.png" alt="Amazon"></a>
+          <a href="#"><img class="socialImg" src="../img/facebook.png" alt="Facebook"></a>
+          <a href="#"><img class="socialImg" src="../img/twitter.png" alt="Twitter"></a>
         </div>
 
         <div class="copyright">
-          <p class="underFooterCopyright">© Copyright 2017 Meat. | Mentions légales | CGV</p>
+          <p class="underFooterCopyright">© Copyright 2018 Meat. | Mentions légales | CGV</p>
         </div>
       </div>
     </div>
