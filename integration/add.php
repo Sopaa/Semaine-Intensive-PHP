90499c4076d1a0db3ce67f58298d6892882d9c08
ayshiff <remi.d45@gmail.com>
2018-02-15 15:03:52 +0100

    Update admin and login route

--- integration/add.php
+++ integration/add.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="meatLogo.ico">
+    <link rel="shortcut icon" href="../img/ViandeLogo.png">
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/register.css">
     <title>Meat</title>
@@ -16,7 +16,7 @@
 
 <body>
 <div class="content">
-    <img class="logo" src="img/ViandeLogo.png" alt="Logo">
+    <img class="logo" src="../img/ViandeLogo.png" alt="Logo">
     <h1 class="title">Inscription.</h1>
     <form action="login.php" method="post">
         <div class="container">
