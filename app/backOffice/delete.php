b274bce66579f905391bb17bba7ba831b0d45335
ayshiff <remi.d45@gmail.com>
2018-02-15 11:22:27 +0100

    Update backoffice

--- app/backOffice/delete.php
+++ app/backOffice/delete.php
@@ -39,7 +39,6 @@ $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -47,15 +46,19 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="shortcut icon" href="../../img/ViandeLogo.png">
+    <link rel="stylesheet" href="../../css/reset.css">
+    <link rel="stylesheet" href="../../css/delete.css">
+    <title>Delete</title>
 </head>
 <body>
-<form action="dodelete.php" method="post">
-    <label for="id"> id : <input type="text" name="id" value="<?=$row['id'] ?>"></label>
-    <label for=""> Êtes-vous sûr de vouloir supprimer <?=$row['nom'] ?> ? </label>
+<h1 class="title">Suppresion d'un élément.</h1>
+<form class="alert" action="dodelete.php" method="post">
+    <input type="hidden" name="id" value="<?=$row['id'] ?>">
+    <label for=""> Êtes-vous sûr de vouloir supprimer cet élément <?=$row['nom'] ?> ? </label>
 
 
-    <input type="submit" value="Supprimer">
+    <input class="submitBtn" type="submit" value="Supprimer">
 </form>
 
 </body>
