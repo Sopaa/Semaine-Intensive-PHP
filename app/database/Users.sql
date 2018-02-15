dd60c6b244ca09929ee72d017d645c5b60321758
ayshiff <remi.d45@gmail.com>
2018-02-15 13:28:29 +0100

    Update data and Guillaume #3

---
+++ app/database/Users.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE Users (
+  id INT(10),
+  nom VARCHAR(255),
+  prenom VARCHAR(255),
+  adresse VARCHAR(255),
+  date_naissance DATE,
+  ville VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+)
---
+++ app/database/loginAdmin.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE loginAdmin (
+  id INT(10) ,
+  email VARCHAR(50),
+  password VARCHAR(50)
+);
+
+INSERT INTO `loginAdmin` VALUES ('1','admin@gmail.com', 'admin');
